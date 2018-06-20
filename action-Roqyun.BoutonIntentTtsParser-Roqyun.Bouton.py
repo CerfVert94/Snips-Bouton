cbf8395370f4e274a0aa0299c5eb3d1dc8c32c66
cerfvert94 <roqyun.ko@etu.upmc.fr>
2018-06-20 16:41:04 +0200

    Modif

--- action-Roqyun.BoutonIntentTtsParser-Roqyun.Bouton.py
+++ action-Roqyun.BoutonIntentTtsParser-Roqyun.Bouton.py
@@ -7,6 +7,9 @@ MQTT_ADDR = "{}:{}".format(MQTT_IP_ADDR, str(MQTT_PORT))
 
 def intent_received(hermes, intent_message):
 	sentence = "Je m'appelle Snips"
+	if intent_message.intent.intent_name == 'Roqyun:Allumage' :
+		sentence += ' que voulez-vous?'
+	
 	hermes.publish_end_session(intent_message.session_id, sentence)
 with Hermes(MQTT_ADDR) as h:
 	h.subscribe_intents(intent_received).start()
