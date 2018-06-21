891e91e601d3a74de636ed0d8f0749919ebc1f1b
cerfvert94 <roqyun.ko@etu.upmc.fr>
2018-06-21 13:01:46 +0200

    Modif

--- action-Roqyun.BoutonIntentTtsParser-Roqyun.Bouton.py
+++ action-Roqyun.BoutonIntentTtsParser-Roqyun.Bouton.py
@@ -1,32 +1,24 @@
 #!/usr/bin/env python2
 from hermes_python.hermes import Hermes
 import json
-#import RPi.GPIO as GPIO
-
-
 
 MQTT_IP_ADDR = "localhost"
 MQTT_PORT = 1883
 MQTT_ADDR = "{}:{}".format(MQTT_IP_ADDR, str(MQTT_PORT))
 
 def intent_received(hermes, intent_message):
-#	GPIO.setwarnings(False);
-#	GPIO.setmode(GPIO.BOARD)
-	
-	#Lire l'objet JSON généré par Snips
+
 	probability = intent_message.intent.probability
 	intentName = intent_message.intent.intent_name
 	
 	if intentName == 'Roqyun:Allumage' :
 		if probability > 0.9 :
 			sentence = "J'allume la lumière"
-			#GPIO.setup(32, GPIO.OUT, initial=GPIO.HIGH)
 		else :
 			sentence = " Je n'ai pas compris"
 	elif intentName == 'Roqyun:Extinction' :
 		if probability > 0.9 :
 			sentence = "J'éteint la lumière"
-			#GPIO.setup(32, GPIO.OUT, initial=GPIO.LOW)
 		else :
 			sentence = " Je n'ai pas compris"
 			
