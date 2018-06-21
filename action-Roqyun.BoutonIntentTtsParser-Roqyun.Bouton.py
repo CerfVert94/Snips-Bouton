2bc6505da6155410e5ee4208a3707688435a3fb8
cerfvert94 <roqyun.ko@etu.upmc.fr>
2018-06-21 10:36:37 +0200

    Modif

--- action-Roqyun.BoutonIntentTtsParser-Roqyun.Bouton.py
+++ action-Roqyun.BoutonIntentTtsParser-Roqyun.Bouton.py
@@ -8,7 +8,6 @@ MQTT_ADDR = "{}:{}".format(MQTT_IP_ADDR, str(MQTT_PORT))
 def intent_received(hermes, intent_message):
 	sentence = "Je m'appelle Snips"
 	probability = intent_message.intent.probability
-	intentName = intent_message.intent.intentName
 	hermes.publish_end_session(intent_message.session_id, sentence)
 with Hermes(MQTT_ADDR) as h:
 	h.subscribe_intents(intent_received).start()
