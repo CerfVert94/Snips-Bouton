78ce5f6b0c7b9e95b2777dd2947183cd901ed5cd
cerfvert94 <roqyun.ko@etu.upmc.fr>
2018-06-21 13:18:34 +0200

    Modif

--- action-Roqyun.BoutonIntentTtsParser-Roqyun.Bouton.py
+++ action-Roqyun.BoutonIntentTtsParser-Roqyun.Bouton.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python2
 from hermes_python.hermes import Hermes
 import json
+import RPi.GPIO as GPIO
 
 MQTT_IP_ADDR = "localhost"
 MQTT_PORT = 1883
 MQTT_ADDR = "{}:{}".format(MQTT_IP_ADDR, str(MQTT_PORT))
 
 def intent_received(hermes, intent_message):
+	GPIO.setwarnings(False);
+	GPIO.setmode(GPIO.BOARD)
+	
 	probability = intent_message.intent.probability
 	intentName = intent_message.intent.intent_name
+	
 	sentence = "J'allume la lumiere"
+	
 	hermes.publish_end_session(intent_message.session_id, sentence)
 with Hermes(MQTT_ADDR) as h:
 	h.subscribe_intents(intent_received).start()
