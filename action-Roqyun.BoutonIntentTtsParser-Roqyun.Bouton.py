92247e147513118e1c1d55debab7737e79d23cc1
cerfvert94 <roqyun.ko@etu.upmc.fr>
2018-06-26 16:22:14 +0200

    Restore backups2

--- action-Roqyun.BoutonIntentTtsParser-Roqyun.Bouton.py
+++ action-Roqyun.BoutonIntentTtsParser-Roqyun.Bouton.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python2
 from hermes_python.hermes import Hermes
-
 MQTT_IP_ADDR = "localhost"
 MQTT_PORT = 1883
 MQTT_ADDR = "{}:{}".format(MQTT_IP_ADDR, str(MQTT_PORT))
 
-def intent_received(hermes, intent_message):	
+def intent_received(hermes, intent_message):
 	probability = intent_message.intent.probability
 	intentName = intent_message.intent.intent_name	
 
 	
 	if intentName == 'Roqyun:Allumage' :
 		if probability > 0.9 :
-			sentence = "Je allume la lumiere"
+			sentence = "J allume la lumiere"
 		else :
 			sentence = " Je n'ai pas compris"
 	elif intentName == 'Roqyun:Extinction' :
@@ -20,8 +19,7 @@ def intent_received(hermes, intent_message):
 			sentence = "Je eteins la lumiere"
 		else :
 			sentence = " Je n'ai pas compris"
-	else :
-		sentence = " Je n'ai pas compris"
+			
 			
 
 	hermes.publish_end_session(intent_message.session_id, sentence)
