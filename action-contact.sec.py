7f2e829c7d42c9b9eee23d270a314dfd83287fcd
cerfvert94 <roqyun.ko@etu.upmc.fr>
2018-06-26 12:56:04 +0200

    Modf

--- action-contact.sec.py
+++ action-contact.sec.py
@@ -1,32 +1,27 @@
 #!/usr/bin/env python2
 from hermes_python.hermes import Hermes
-import RPi.GPIO as GPIO
 
 MQTT_IP_ADDR = "localhost"
 MQTT_PORT = 1883
 MQTT_ADDR = "{}:{}".format(MQTT_IP_ADDR, str(MQTT_PORT))
 
-def intent_received(hermes, intent_message):
-	GPIO.setwarnings(False);
-	GPIO.setmode(GPIO.BOARD)
-	
+def intent_received(hermes, intent_message):	
 	probability = intent_message.intent.probability
 	intentName = intent_message.intent.intent_name	
 
 	
 	if intentName == 'Roqyun:Allumage' :
 		if probability > 0.9 :
-			sentence = "J allume la lumiere"
-			GPIO.setup(32, GPIO.OUT, initial=GPIO.HIGH)
+			sentence = "Je allume la lumiere"
 		else :
 			sentence = " Je n'ai pas compris"
 	elif intentName == 'Roqyun:Extinction' :
 		if probability > 0.9 :
 			sentence = "Je eteins la lumiere"
-			GPIO.setup(32, GPIO.OUT, initial=GPIO.LOW)
 		else :
 			sentence = " Je n'ai pas compris"
-			
+	else :
+		sentence = " Je n'ai pas compris"
 			
 
 	hermes.publish_end_session(intent_message.session_id, sentence)
